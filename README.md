846286f8520e97eedf22a07698b3a5a044718c21
Blueibis <jennie.li.2011@gmail.com>
2017-07-10 20:20:22 -0400

    everything but the stretch :/

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Your First (Several) Asynchronous Requests
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/cohorts/67/assignments/2055/latest)
+#Step 0
+1) What HTTP method did your browser use to make the request?
+  GET
+2) How many milliseconds did it take your browser to complete it?
+  61.47 ms request/response
+3) What HTTP status code did the server return? What does that mean?
+  200 OK, success
+4) Look at the "Initiator" and "Type" columns. Was this an xhr/JavaScript/AJAX request or a normal browser request?
+  browser request
+#Step 1
--- ajax.js
+++ ajax.js
@@ -1,5 +1,74 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   /* Your code goes here */
+  var button1 = document.querySelector('#step2');
+  var button2 = document.querySelector('#step3');
+  var button3 = document.querySelector('button#step7');
+  var button4 = document.querySelector('button#step8');
+  var button5 = document.querySelector('button#step9');
+
+  button1.addEventListener('click', function() {
+    $.ajax( {
+      url: 'http://first-ajax-api.herokuapp.com/',
+      method: 'GET',
+      dataType: 'html',
+    } );
+  });
+
+  button2.addEventListener('click', function() {
+    $.ajax( {
+      url: 'http://first-ajax-api.herokuapp.com/ping',
+      method: 'GET',
+      dataType: 'html',
+    }).done(function(responseData) {
+      console.log(responseData);
+      var response = document.createElement('p');
+      response.innerHTML = responseData;
+      document.querySelector('#step3456').append(response);
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+      var failMessage = document.createElement('p');
+      failMessage.innerHTML = 'Something went wrong! We\'ll try out best to fix this, sorry!'
+      document.querySelector('#step3456').append(failMessage);
+    }).always(function() {
+      console.log('Request completed!!!')
+    });
+  });
+
+  button3.addEventListener('click', function() {
+    $.ajax( {
+      url: 'http://first-ajax-api.herokuapp.com/count',
+      method: 'GET',
+      dataType: 'text',
+    }).done(function(response) {
+      var count = document.createElement('p');
+      count.innerHTML = response;
+      document.querySelector('section#step7').append(count);
+    })
+  });
+
+  button4.addEventListener('click', function() {
+    $.ajax( {
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      method: 'GET',
+      data: { timezone: 'Pacific/Honolulu' },
+      dataType: 'text',
+    }).done(function(response) {
+      var time = document.createElement('p');
+      time.innerHTML = response;
+      document.querySelector('section#step8').append(time);
+    })
+  });
+
+  button5.addEventListener('click', function() {
+    $.ajax( {
+      url: 'http://first-ajax-api.herokuapp.com/a_car',
+      method: 'GET',
+      dataType: 'html',
+    }).done(function(response) {
+      var car = document.createElement('div');
+      car.innerHTML = response;
+      document.querySelector('#list').append(car);
+    })
+  });
 
 });
